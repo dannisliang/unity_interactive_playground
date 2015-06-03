9f37802e8707aac7f8390f2f4b703f03163e8b42
gigglesninja <jct322@msstate.edu>
2015-06-03 16:26:42 -0500

    added roll a ball game

---
+++ lets_go/Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+    public float speed;
+    public float xangle, yangle, zangle;
+
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.Rotate(new Vector3(xangle, yangle, zangle) * Time.deltaTime * speed);   
+	}
+}
