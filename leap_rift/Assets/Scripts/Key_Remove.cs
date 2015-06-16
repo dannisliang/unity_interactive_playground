8cc9a60ccd4ccf0cf0e2fa7bfcb37b591d106a6c
gigglesninja <jct322@msstate.edu>
2015-06-16 01:46:05 -0500

    finilized demo v1

---
+++ leap_rift/Assets/Scripts/Key_Remove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Key_Remove : MonoBehaviour {
+    public KeyCode toggle = KeyCode.T;
+	private bool isActive = true;
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetKeyDown(KeyCode.Z))
+    {
+      isActive = !isActive;
+      foreach (Transform child_transform in transform)
+      {
+        child_transform.gameObject.SetActive(isActive);
+      }
+    }
+	}
+}
