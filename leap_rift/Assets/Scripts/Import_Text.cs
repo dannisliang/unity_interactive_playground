f7e000c8b721d0c6b8fb76ff97d1b9012cb6b0cf
gigglesninja <jct322@msstate.edu>
2015-06-17 02:21:12 -0500

    added work bench

--- leap_rift/Assets/Scripts/Import_Text.cs
+++ leap_rift/Assets/Scripts/Import_Text.cs
@@ -4,11 +4,13 @@ using System.Collections;
 
 public class Import_Text : MonoBehaviour {
     Text text;
+    public int fontSize;
 
     void FixedUpdate()
     {
         string txt = System.IO.File.ReadAllText(@"C:\Users\James\Desktop\main.txt");
         text = GetComponent<Text>();
         text.text = txt;
+        GetComponent<Text>().fontSize = fontSize;
     }
 }
