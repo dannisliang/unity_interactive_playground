cfed6c2db5076f81ab7b82dea74417c32ac4436d
gigglesninja <jct322@msstate.edu>
2015-06-18 19:38:14 -0500

    added scripts to move room and rotate text

---
+++ leap_rift/Assets/Scripts/Circle_Gesture.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class Circle_Gesture : MonoBehaviour {
+    Controller controller;
+    public float minRadius = 100;
+    public float minArc = 3.14f;
+
+    public GameObject whatToSpin;
+
+    void Start()
+    {
+        controller = new Controller();
+        controller.EnableGesture(Gesture.GestureType.TYPE_CIRCLE);
+        controller.Config.SetFloat("Gesture.Circle.MinRadius", minRadius);
+        controller.Config.SetFloat("Gesture.Circle.MinArc", minArc);
+        controller.Config.Save();
+    }
+
+    void Update()
+    {
+        Frame frame = controller.Frame();
+        GestureList gesturelist = frame.Gestures();
+        for (int i = 0; i < gesturelist.Count; i++)
+        {
+            Gesture gesture = gesturelist[i];
+            if (gesture.Type == Gesture.GestureType.TYPECIRCLE)
+            {
+                Debug.Log("circle");
+                whatToSpin.transform.Rotate(Vector3.up, 90 * Time.deltaTime);
+            }
+        }
+
+            /*for (int i = 0; i < gesturelist.Count; i++)
+            {
+                Gesture gesture = gesturelist[i];
+                if (gesture.Type == Gesture.GestureType.TYPESWIPE)
+                {
+                    SwipeGesture swipe = new SwipeGesture(gesture);
+                    Vector swipeDirection = swipe.Direction;
+                    if (swipeDirection.x < 0)
+                    {
+                        Debug.Log("Left");
+                        whatToSpin.transform.Rotate(Vector3.up, spinAngle * Time.deltaTime);
+                    }
+                    if (swipeDirection.x > 0)
+                    {
+                        Debug.Log("Right");
+                        whatToSpin.transform.Rotate(Vector3.up, -spinAngle * Time.deltaTime);
+                    }
+                }
+            }*/
+    }
+}
